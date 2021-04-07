41960ceb71316a8174fd2fb7456762731336b36c
evolvingkid <fayezmohammed23@gmail.com>
2021-04-07 10:22:04 +0530

    user change

--- model/user.js
+++ model/user.js
@@ -4,22 +4,55 @@ var uniqueValidator = require('mongoose-unique-validator');
 
 
 const UsersModel = new mongoose.Schema({
-    name: { type: String, required:true,  },
-    password: { type: String, required:true },
-    email: {type: String, unique: true},
-    designation : { type: String, required:true },
-    priviliage : []
-});
+  firstName: { type: String, required: true, },
+  listName: { type: String, required: true },
+  email: { type: String, unique: true },
+  dob: { type: Date, required: true },
+  aadhar: { type: String, required: true },
+  communicationAddress: {
+    country: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    district: {
+      type: String,
+      required: true
+    },
+    city : {
+      type: String,
+      required: true
+    },
+    house : {
+      type: String,
+      required: true,
+    },
+    pincode:{
+      type: Number,
+      required: true
+    },
+  },
+  priviliage: [],
+
+},
+  {
+    timestamps: true
+  }
+);
 
 UsersModel.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-      if (err) {
-        return next(err); }
-      user.password = hash;
-      next();
-    })
-  });
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
 
 UsersModel.plugin(uniqueValidator, { message: '{PATH} already exists!' });
 
