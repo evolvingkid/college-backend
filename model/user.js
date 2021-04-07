02c138bc811cf8be82e1c3a24ce0540dd5a8ab21
trojancode <codeboycodes@gmail.com>
2021-04-06 21:29:54 -0700

    User mpdel demo

--- model/user.js
+++ model/user.js
@@ -1,27 +1,68 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-var uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
 
-
-const UsersModel = new mongoose.Schema({
-    name: { type: String, required:true,  },
-    password: { type: String, required:true },
-    email: {type: String, unique: true},
-    designation : { type: String, required:true },
-    priviliage : []
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: 100
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: 13
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+}, {
+    timestamps: true
 });
 
-UsersModel.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-      if (err) {
-        return next(err); }
-      user.password = hash;
-      next();
+userSchema.virtual('password')
+    
+    .set(function (password) {
+        console.log(password);
+        this._password = password,
+        this.salt = uuidv1(),
+        this.hashed_password = this.encryptPassword(password)
     })
-  });
+    .get(function () {
+        return this.password
+    })
 
-UsersModel.plugin(uniqueValidator, { message: '{PATH} already exists!' });
+userSchema.methods = {
+
+    authenticate: function(password){
+        return this.encryptPassword(password) === this.hashed_password;
+    },
 
 
-module.exports = User = mongoose.model('UsersModel', UsersModel);
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        }catch(err){
+            return "";
+        }
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
