2f154feec714b75b4a975c6fe76c6757fb00d02c
evolvingkid <fayezmohammed23@gmail.com>
2021-04-30 11:29:52 +0530

    added description

--- model/department.js
+++ model/department.js
@@ -1,34 +1,41 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const DepartmentModel = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    hods: [
-        {
-            hod: {
-                type: mongoose.Schema.Types.ObjectId,
-                requird: true,
-                ref: "user"
-            },
-            startYear : {
-                type : Date,
-                required : true
-            },
-            endYear : {
-                type : Date
-            }
-        },
-    ],
-    startYear: {
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  hods: [
+    {
+      hod: {
+        type: mongoose.Schema.Types.ObjectId,
+        requird: true,
+        ref: "user",
+      },
+      startYear: {
         type: Date,
         required: true,
+      },
+      endYear: {
+        type: Date,
+      },
     },
+  ],
+  startYear: {
+    type: Date,
+    required: true,
+  },
 });
 
-DepartmentModel.plugin(uniqueValidator, { message: '{PATH} already exists!' });
+DepartmentModel.plugin(uniqueValidator, { message: "{PATH} already exists!" });
 
-module.exports = Department = mongoose.model('DepartmentModel', DepartmentModel);
+module.exports = Department = mongoose.model(
+  "DepartmentModel",
+  DepartmentModel
+);
+
